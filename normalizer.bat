8d35dc8a440e4059f0ae2a5e6be0b7d40f57088d
Heavenanvil <heavenanvil@gmail.com>
2020-06-12 09:28:45 +0700

    update 1.2
    • Добавлена команда смены кодировки на UTF-8, для совместимости с кириллическими именами файлов.
    • Имена переменных, при их объявлении, теперь взяты в кавычки, для избежания проблем с пробелами в именах файлов.

--- normalizer.bat
+++ normalizer.bat
@@ -2,7 +2,7 @@
 :: 
 :: © Heavenanvil
 :: E-mail: heavenanvil@gmail.com
-:: version 1.1
+:: version 1.2
 ::
 :: данный скрипт при помощи консольного приложения ffmpeg позволяет
 :: из mkv-видеофайла вытащить звуковую дорожку ac3, 
@@ -13,44 +13,47 @@
 :: Ссылка на актуальный ffmpeg.exe
 :: https://ffmpeg.zeranoe.com/builds/
 
+:: Меняем кодировку на UTF-8, для совместимости с кириллическими именами файлов.
+chcp 65001
+
 :: Объявляем переменные
-set file=myfavoritefilm
-set format=mkv
-set aformat=ac3
+set file="filename"
+set format="mkv"
+set aformat="ac3"
 
 :: Шаг 1 - Извлекаем отдельно видео дорожку
-:: ffmpeg -i "%file%.%format%" -y -an -vcodec copy "%file%+.%format%"
-ffmpeg -i "%file%.%format%" -map 0:v -c copy "%file%+.%format%"
+:: ffmpeg -i %file%.%format% -y -an -vcodec copy %file%+.%format%
+ffmpeg -i %file%.%format% -y -map 0:v -c copy %file%+.%format%
 
 :: Шаг 2 - Извлекаем первую аудиодорожку
-:: ffmpeg -i "%file%.%format%" -y -c:a copy "%file%.%aformat%"
-ffmpeg -i "%file%.%format%" -map 0:a -c copy %file%.%aformat%"
+:: ffmpeg -i %file%.%format% -y -c:a copy %file%.%aformat%
+ffmpeg -i %file%.%format% -y -map 0:a -c copy %file%.%aformat%
 
 :: Шаг 3 - %aformat% 5.1 кодируем в стерео (2.0)
-ffmpeg -i "%file%.%aformat%" -y -c:a %aformat% -ac 2 "%file%+.%aformat%"
+ffmpeg -i %file%.%aformat% -y -c:a %aformat% -ac 2 %file%+.%aformat%
 
 :: Шаг 4 - Применяем Limiter -6dB
-ffmpeg -i "%file%+.%aformat%" -y -filter_complex alimiter=level_in=1:level_out=1:limit=-6dB:attack=7:release=100:level=disabled "%file%++.%aformat%"
+ffmpeg -i %file%+.%aformat% -y -filter_complex alimiter=level_in=1:level_out=1:limit=-6dB:attack=7:release=100:level=disabled %file%++.%aformat%
 
 :: Шаг 5 - Добавляем громкости на 7dB
-ffmpeg -i "%file%++.%aformat%" -y -filter:a "volume=7dB" "%file%+++.%aformat%"
+ffmpeg -i %file%++.%aformat% -y -filter:a "volume=7dB" %file%+++.%aformat%
 
 :: Шаг 6 - Применяем Limiter -8dB
- ffmpeg -i "%file%+++.%aformat%" -y -filter_complex alimiter=level_in=1:level_out=1:limit=-8dB:attack=7:release=100:level=disabled "%file%++++.%aformat%"
+ ffmpeg -i %file%+++.%aformat% -y -filter_complex alimiter=level_in=1:level_out=1:limit=-8dB:attack=7:release=100:level=disabled %file%++++.%aformat%
 
 :: Шаг 7 - Добавляем громоксти на 4dB
-ffmpeg -i "%file%++++.%aformat%" -y -filter:a "volume=4dB" "%file%+++++.%aformat%"
+ffmpeg -i %file%++++.%aformat% -y -filter:a "volume=4dB" %file%+++++.%aformat%
 
 :: Шаг 8 - Собираем контейнер обратно
-ffmpeg -i "%file%+.%format%" -i "%file%+++++.%aformat%" -c:v copy -c:a copy -map 0:v:0 -map 1:a:0 "%file%_norm.%format%"
+ffmpeg -i %file%+.%format% -i %file%+++++.%aformat% -y -c:v copy -c:a copy -map 0:v:0 -map 1:a:0 %file%_norm.%format%
 
 :: Шаг 9 - Удаляем лишние файлы
-del "%file%+.%format%" -y
-del "%file%.%aformat%" -y
-del "%file%+.%aformat%" -y
-del "%file%++.%aformat%" -y
-del "%file%+++.%aformat%" -y
-del "%file%++++.%aformat%" -y
-del "%file%+++++.%aformat%" -y
+del %file%+.%format% -y
+del %file%.%aformat% -y
+del %file%+.%aformat% -y
+del %file%++.%aformat% -y
+del %file%+++.%aformat% -y
+del %file%++++.%aformat% -y
+del %file%+++++.%aformat% -y
 
 pause
